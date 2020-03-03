c8355a56caca567fee1e0a568848903d482a2c16
Tommy Meisel <thmeisel@microsoft.com>
2020-03-02 20:51:17 -0800

    make enum name function public

--- compiler/cpp/src/thrift/generate/t_swift_generator.cc
+++ compiler/cpp/src/thrift/generate/t_swift_generator.cc
@@ -444,15 +444,15 @@ void t_swift_generator::generate_enum(t_enum* tenum) {
   }
 
   if (telemetry_object_) {
-    f_impl_ << indent() << "fileprivate func telemetryValue() -> TelemetryValue";
+    f_impl_ << indent() << "public func telemetryName() -> String";
     block_open(f_impl_);
     if (boost::algorithm::ends_with(tenum->get_name(), "AsInt")) {
-      f_impl_ << indent() << "return .string(\"\\(rawValue)\")" << endl;
+      f_impl_ << indent() << "return \"\\(rawValue)\"" << endl;
     }
     else {
       f_impl_ << indent() << "switch self {" << endl;
       for (const auto& value : tenum->get_constants()) {
-        f_impl_ << indent() << "case ." << enum_value_name(value) << ": return .string(\"" << value->get_name() << "\")" << endl;
+        f_impl_ << indent() << "case ." << enum_value_name(value) << ": return \"" << value->get_name() << "\"" << endl;
       }
       f_impl_ << indent() << "}" << endl;
     }
@@ -750,7 +750,7 @@ void t_swift_generator::generate_swift_struct_telemetry_object_extension(ofstrea
 
   out << indent() << "public func telemetryDictionary() -> TelemetryDictionary";
   block_open(out);
- 
+
   out << endl;
 
   out << indent() << "var telemetryData = TelemetryDictionary()" << endl;
@@ -764,7 +764,7 @@ void t_swift_generator::generate_swift_struct_telemetry_object_extension(ofstrea
 
     out << indent() << "telemetryData[\"" << member->get_name() << "\"] = ";
     if (member->get_type()->is_enum()) {
-      out << struct_property_name(member) << ".telemetryValue()";
+      out << ".string(" << struct_property_name(member) << ".telemetryName())";
     }
     else {
       out << "TelemetryValue(" << struct_property_name(member) << ")";
@@ -777,7 +777,7 @@ void t_swift_generator::generate_swift_struct_telemetry_object_extension(ofstrea
   }
 
   out << indent() << "return telemetryData" << endl;
-  
+
   block_close(out);
   block_close(out);
 
@@ -2281,7 +2281,7 @@ string t_swift_generator::struct_property_name(t_field* tfield) {
 string t_swift_generator::enum_value_name(t_enum_value* tenumvalue) {
   return maybe_escape_identifier(camel_case_from_underscore(tenumvalue->get_name()));
 }
- 
+
 string t_swift_generator::camel_case_from_underscore(string underscore_name) {
   // Convert to camel case
   std::string cap_value_name = underscore_name;
