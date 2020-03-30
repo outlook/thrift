9f74ac8d67285497c7533ea6ba5a29273a770278
Tommy Meisel <thmeisel@microsoft.com>
2020-03-30 15:49:06 -0700

    add exceptions

--- compiler/cpp/src/thrift/lint/t_linter.cc
+++ compiler/cpp/src/thrift/lint/t_linter.cc
@@ -17,6 +17,7 @@
  * under the License.
  */
 
+#include <algorithm>
 #include <iostream>
 #include <regex>
 #include "thrift/lint/t_linter.h"
@@ -81,6 +82,116 @@ bool t_linter::validate_struct_names() {
 }
 
 bool t_linter::validate_enum_constant_names() {
+  vector<string> enum_exceptions = {
+    "OTPrivacyLevel",
+    "OTDiagnosticConsentLevelAsInt",
+    "OTPrivacyServiceStateAsInt",
+    "OTPrivacySettingSourceLocationAsInt",
+    "OTPrivacyIdentitySpace",
+    "OTCIDType",
+    "OTAccountType",
+    "OTAccountCloud",
+    "OTFolderType",
+    "OTPrivacyAccountType",
+    "OTPrivacySettingsAction",
+    "OTPrivacySettingsResult",
+    "OTPrivacySettingType",
+    "OTPrivacyRoamingService",
+    "OTPrivacySettingsFailureReason",
+  };
+
+  vector<string> exceptions = {
+    "underSubmit",
+    "bottomOfScreen",
+    "actionCard",
+    "hideAd",
+    "GROUPS",
+    "OTHER",
+    "GROUP",
+    "OTHER",
+    "fileDetails",
+    "unifiedInboxHeader",
+    "singleInboxHeader",
+    "keyStroke",
+    "NoResponse",
+    "Organizer",
+    "Tentative",
+    "Accepted",
+    "Declined",
+    "ProposedNewTime",
+    "Accept",
+    "Reply",
+    "Edit",
+    "NoResponse",
+    "Folder",
+    "Group",
+    "Persona",
+    "Unknown",
+    "missing_xAnchor_mailbox",
+    "Mail",
+    "Calendar",
+    "clearButton",
+    "contextChanged",
+    "keyboardInput",
+    "zeroQuery",
+    "includeDeletedToggle",
+    "voiceAssistant",
+    "spellerSuggestion",
+    "closedWithSearch",
+    "closedWithoutSearch",
+    "closedWithTimeout",
+    "closedWithError",
+    "deniedPermissions",
+    "latencyDiffFirstWord",
+    "startedListening",
+    "microphoneShown",
+    "initializingStateShown",
+    "peoplePhone",
+    "peopleEmail",
+    "peopleOfficeLocation",
+    "MFU",
+    "MRU",
+    "microsoftGraph",
+    "lokiAccessToken",
+    "substrateToken",
+    "cortanaToken",
+    "featureAwareness",
+    "newAccount",
+    "changeServerSettings",
+    "timeoutError",
+    "authenticationError",
+    "unknownError",
+    "qrIntroShown",
+    "qrIntroScanQrCode",
+    "qrIntroSignInManually",
+    "qrScanShown",
+    "qrScanSignInManually",
+    "qrScanSuccess",
+    "qrScanFail",
+    "lensSDK_camera",
+    "enterBackgroundEarly",
+    "remoteNotification",
+    "significantTimeChange",
+    "backgroundFetch",
+    "archiveAndMarkAsRead",
+    "messageDetail",
+    "messageListBulkAction",
+    "messageListSwipe",
+    "ComposeAttachment",
+    "ComposeInline",
+    "ComposeRecipient",
+    "Attachment",
+    "MessagesList",
+    "MessageListInSearchResults",
+    "FilePicker",
+    "FilesInZeroQuery",
+    "EventsAgendaView",
+    "EventsDayView",
+    "ContactsInZeroQuery",
+    "PeopleList",
+    "BE",
+    "FE",
+  };
   std::regex regex(R"(^[a-z0-9_]+$)");
   bool contains_failure = false;
 
@@ -89,13 +200,22 @@ bool t_linter::validate_enum_constant_names() {
   for (e_iter = enums.begin(); e_iter != enums.end(); ++e_iter) {
     t_enum* en = *e_iter;
 
+    if (std::find(enum_exceptions.begin(), enum_exceptions.end(), en->get_name()) != enum_exceptions.end()) {
+      continue;
+    }
+
     vector<t_enum_value*> constants = en->get_constants();
     vector<t_enum_value*>::iterator c_iter;
 
     for (c_iter = constants.begin(); c_iter != constants.end(); ++c_iter) {
 
-      if (!std::regex_match((*c_iter)->get_name(), regex)) {
-        cout << "Failed regex for enum constant name: " << (*c_iter)->get_name() << endl;
+      string name = (*c_iter)->get_name();
+      if (std::find(exceptions.begin(), exceptions.end(), name) != exceptions.end()) {
+        continue;
+      }
+
+      if (!std::regex_match(name, regex)) {
+        cout << "Failed regex for enum constant name: " << name << endl;
         contains_failure = true;
       }
     }
